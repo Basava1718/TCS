01eed7e9e78db167baeb24149a113a0e94b3ebb2
Basava1718 <bbolmall4@gmail.com>
2025-09-27 15:56:09 +0530

    Update App.java

--- src/main/java/TCS/App.java
+++ src/main/java/TCS/App.java
@@ -5,7 +5,6 @@ package TCS;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        http://65.2.170.80:8080/
+        System.out.println("Hello World!")
             }
 }
